2cae07527b38a61f3894fba77e34f001a5d94fc6
Del <mayhakd@cpenow.com>
2016-07-20 13:59:04 -0600

    Changed Helpers namespace

--- DbContext/Core/Helpers.cs
+++ DbContext/Core/Helpers.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HyperSlackers.AspNet.Identity.EntityFramework
+namespace HyperSlackers
 {
     internal static class Helpers
     {
